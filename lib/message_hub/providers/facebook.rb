e05870897dfb8bfba7cd664e423d715b912c6d24
David A. Cuadrado <krawek@gmail.com>
2012-10-08 23:58:03 -0500

    Implementing facebook provider.

--- lib/message_hub/providers/facebook.rb
+++ lib/message_hub/providers/facebook.rb
@@ -1,6 +1,29 @@
 module MessageHub
   module Providers
     class Facebook < MessageHub::Provider
+      #####
+      # Login to gmail
+      # options are:
+      #   :access_token
+      def login(credentials = {})
+        @client = Koala::Facebook::API.new(credentials[:access_token])
+      end
+
+      #####
+      # Fetch messages from gmail
+      # options are:
+      #
+      def fetch_messages(opts = {}, &block)
+      end
+
+      #####
+      # Send message via gmail
+      # options are:
+      #   :message
+      #
+      def send_message(opts = {})
+        @client.put_connections("me", "feed", :message => opts[:message])
+      end
     end
   end
 end
