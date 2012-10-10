0b2ca06dab149993931ca2a5dce4cb48e0738c5c
David A. Cuadrado <krawek@gmail.com>
2012-10-09 23:43:20 -0500

    Facebook provider is ready to use.

--- lib/message_hub/providers/facebook.rb
+++ lib/message_hub/providers/facebook.rb
@@ -12,17 +12,55 @@ module MessageHub
       #####
       # Fetch messages from gmail
       # options are:
+      #   :page_id
+      #   :since
+      #   :search
+      #
       #
       def fetch_messages(opts = {}, &block)
+        opts[:since] = opts[:since].to_i if opts[:since].kind_of?(Time)
+        filter = opts.delete(:search)
+        if filter
+          filter = Regexp.new(filter.split(/\s+/).join("|"), Regexp::IGNORECASE)
+        end
+
+        feed = @client.get_connections(opts.delete(:page_id), "feed", opts)
+        loop do
+          feed.each do |item|
+            if !item['message']
+              next
+            end
+
+            message_data = {
+              :id => item['id'],
+              :source => 'facebook',
+              :thread => item['id'],
+              :body => item['message'],
+              :sender_name => item['from']['name'],
+              :sender => item['from']['id']
+            }
+
+            if !filter
+              block.call Message.build(message_data)
+            elsif message_data[:body] =~ filter
+              block.call Message.build(message_data)
+            end
+
+          end
+          feed = feed.next_page
+
+          break if feed.nil?
+        end
       end
 
       #####
       # Send message via gmail
       # options are:
+      #   :page_id
       #   :message
       #
       def send_message(opts = {})
-        @client.put_connections("me", "feed", :message => opts[:message])
+        @client.put_connections(params[:page_id], "feed", :message => opts[:message])
       end
     end
   end
